8c441f77cb5ea735219964eacfafedd6c73cf9a1
ofachati <42001547@parisnanterre.fr>
2022-10-24 17:34:09 +0200

    exercice 2 question 2 last version

--- EX2-TD2-PF-EL_FACHATI_Omar/src/main/java/Main.java
+++ EX2-TD2-PF-EL_FACHATI_Omar/src/main/java/Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
+        Exercice2 ex = new Exercice2();
+        Paire testPetit = new Paire(80, 80.0);
+        System.out.println("T: 80 , P: 80.0  :"+ex.accesOK.test(testPetit));
+
+        Paire testGrand = new Paire(250, 80.0);
+        System.out.println("T: 250 , P: 80.0 :"+ex.accesOK.test(testGrand));
+
 
     }
 }
