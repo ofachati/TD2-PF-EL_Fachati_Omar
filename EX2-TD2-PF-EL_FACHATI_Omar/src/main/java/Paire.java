30b9e8ea39974cfa72d6b6b20a9daf269314894d
ofachati <42001547@parisnanterre.fr>
2022-10-24 17:11:24 +0200

    exercice 2 question 1

--- EX2-TD2-PF-EL_FACHATI_Omar/src/main/java/Paire.java
+++ EX2-TD2-PF-EL_FACHATI_Omar/src/main/java/Paire.java
@@ -19,11 +19,15 @@ public class Paire<T,U> {
 
 
     //question 1
-    Predicate<Paire<Integer, Double>> ttp = p -> p.fst < 100; // appel à la paire avec la fonction fst
-    Predicate<Paire<Integer, Double>> ttg = p -> p.fst > 200; // appel à la paire avec la fonction fst
+    Predicate<Paire<Integer, Double>> ttp = p -> p.fst < 100;
+    Predicate<Paire<Integer, Double>> ttg = p -> p.fst > 200;
     Predicate<Paire<Integer, Double>> tailleIncorrecte = p -> ttp.test(p) || ttg.test(p);
     Predicate<Paire<Integer, Double>> tailleCorrecte = Predicate.not(tailleIncorrecte);
     Predicate<Paire<Integer, Double>> ptl = p -> p.snd > 150.0;
     Predicate<Paire<Integer, Double>> poidCorrect = Predicate.not(ptl);
     Predicate<Paire<Integer, Double>>  accesOK= p -> tailleCorrecte.test(p) && poidCorrect.test(p);
+
+  
+
+
 }
