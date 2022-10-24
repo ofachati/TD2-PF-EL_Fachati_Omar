4642970e75b39f84be61df37a2fe10ecc1d4cc38
ofachati <42001547@parisnanterre.fr>
2022-10-24 15:49:08 +0200

    exercice 1 question 1

--- EX1-Interfaces_fonctionnelles/EX1-TD2-PF-EL_FACHATI_Omar/src/main/java/Main.java
+++ EX1-Interfaces_fonctionnelles/EX1-TD2-PF-EL_FACHATI_Omar/src/main/java/Main.java
@@ -5,9 +5,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
-    Somme< Integer> addition = (a, b) -> a + b;
-
-    System.out.println(" 6 + 8 = " + addition.sommer(   6, 8));
+    Somme< Integer> addition_int = (a, b) -> a + b;
+    System.out.println(" 6 + 8 = " + addition_int.sommer(   6, 8));
+    Somme<Double> addition_double =  (a, b) -> a + b;
+    Somme<Long> addition_long =  (a, b) -> a + b;
+    Somme<String> addition_string =  (a, b) -> a + b;
+    System.out.println(" 6 + 8 = " + addition_int.sommer(   6, 8));
 
     }
 }
